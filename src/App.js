164fa69970ab83a8880da5f5da35b10c7d9596ea
Mr Sherry <mr.sherry.793@gmail.com>
2023-08-13 21:58:26 +0500

    before designing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import Login from "./pages/Login";
+import { hashParams } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./pages/Player";
+import { useDataLayerValue } from "./context/DataLayer";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // const [token, setToken] = useState(null);
+  const [{ user,token }, dispatch] = useDataLayerValue();
+
+  useEffect(() => {
+    const hash = hashParams();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    console.log("my token value is :", _token);
+
+    if (_token) {
+      // setToken(_token);
+      dispatch({
+        type:"SET_TOKEN",
+        token:_token
+      })
+      spotify.setAccessToken(_token);
+
+      spotify
+        .getMe()
+        .then((user) => {
+          // console.log("🧑", user);
+          dispatch({
+            type: "SET_USER",
+            user: user,
+          });
+        })
+        .catch((err) => {
+          console.log("Error", err);
+        });
+      }
+    }, []);
+
+    console.log("🧑", user);
+    console.log("😎", token);
+
+  return <div className="app">{token ? <Player spotify={spotify} /> : <Login />}</div>;
 }
 
 export default App;
---
+++ src/pages/Player.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Player.css"
+
+function Player({spotify}) {
+  // console.log(spotify)
+  return (
+    <div><h1>Welcome to the spotify</h1></div>
+  )
+}
+
+export default Player
